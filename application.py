300e6115b0f5a4bcca8aadccd3c56e9218d3a331
luckyj-lab <jamesw3517@gmail.com>
2021-10-04 16:23:53 -0500

    new modification to application.py

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 from flask import Flask
 application = Flask(__name__)
 
+app = application
 @application.route("/")
 def hello_world():
     return "Please subscribe to my webpage where ever you are"
 
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run(debug=False)
